081287e88c96bcbbad2e7537b0295076924ab048
nataliaarevalo3 <nataliaarevalo440@gmail.com>
2024-04-04 23:32:50 -0600

    guardar en repositorio

--- Calificaciones.js
+++ Calificaciones.js
@@ -1,14 +1,20 @@
-function verificarAprobacion(calificacion, asistencias) {
-    if (asistencias >= 80) {
-        if (calificacion >= 5) { 
+function verificarAprobacion(calificacion, asistencias, Total_clases) {
+    var porcentaje = (asistencias / Total_clases) * 100;
+
+    if (porcentaje >= 80) {
+        if (calificacion >= 3) { 
             return "Aprobado";
         } else {
             return "Reprobado";
         }
+    } else {
+        return "Reprobado por inasistencias";
+    }
 }
-}
+
 let calificacion = parseFloat(prompt("Ingrese la calificación del alumno: "));
 let asistencias = parseFloat(prompt("Ingrese el porcentaje de asistencias del alumno: "));
+let Total_clases = parseFloat(prompt("Ingrese el número total de clases: "));
 
-let resultado = verificarAprobacion(calificacion, asistencias);
+let resultado = verificarAprobacion(calificacion, asistencias, Total_clases);
 alert("El alumno está " + resultado);
